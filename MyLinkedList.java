6d267ef0fd431268030edd40bfa0598496299fc0
Allen Wang <awang02@stuy.edu>
2018-12-14 03:04:52 -0500

    Updated Node class methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,6 +7,37 @@ public class MyLinkedList {
   public boolean add(int value){}
   public String toString(){}
 
+  private class Node {
+    private Integer data;
+    private Node next,prev;
 
+    public Node(Integer data, Node next, Node prev) {
+      this.data = data;
+      this.next = next;
+      this.prev = prev;
+    }
+
+    public Node next(){
+      return next;
+    }
+    public Node prev(){
+      return prev;
+    }
+    public void setNext(Node other){
+      next = other;
+    }
+    public void setPrev(Node other){
+      prev = other;
+    }
+    public Integer getData(){
+      return data;
+    }
+    public Integer setData(Integer i){
+      Integer x = data;
+      data = n
+      return x;
+    }
+    public String toString()
   }
+
 }
